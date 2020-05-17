f5bcb65aff9fb0a34c3f7c1233f8e37c4eb24a4a
flekystyley <flekystyley@gmail.com>
2020-05-18 01:45:00 +0900

    fix;

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,10 @@ fn main() -> io::Result<()> {
     /*
      * Create Virtual Interface.
      */
-    let nic = tun_tap::new("tun_0", tun_tap::Mode::Tun).except("failed to virtual interface")?;
+    let nic = tun_tap::Iface::new("tun_0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x}", nbytes, &buf[..nbytes]);
-
-    /* TODO must resolve error
-     * running: "cc" "-O3" "-ffunction-sections" "-fdata-sections" "-fPIC" "-m64" "-Wall" "-Wextra" "-o" "/Users/flekystyley/Source/tcp-rust/target/release/build/tun-tap-f4c3be278150ae9c/out/src/tuntap.o" "-c" "src/tuntap.c"
-        cargo:warning=src/tuntap.c:11:10: fatal error: 'linux/if.h' file not found
-        cargo:warning=#include <linux/if.h>
-        cargo:warning=         ^~~~~~~~~~~~
-        cargo:warning=1 error generated.
-        exit code: 1
-
-     * 多分だけどlinxカーネルのversionが古いか,if.hが入ってない(これはどのライブラリに依存してる？)
-    */
+    eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
 
     Ok(())
 }
