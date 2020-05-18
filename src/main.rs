4c90ec0b51945a5b51eef59db6d1ae4cc4974ae7
flekystyley <flekystyley@gmail.com>
2020-05-18 18:14:38 +0900

    fix;

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ fn main() -> io::Result<()> {
      */
     let nic = tun_tap::Iface::new("tun_0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
-
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    }
     Ok(())
 }
