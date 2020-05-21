2445b2f4883c87b32779294aeaf56714060a6d6f
flekystyley <flekystyley@gmail.com>
2020-05-21 21:30:23 +0900

    apply ipv4;

--- run_tun.sh
+++ run_tun.sh
@@ -1,8 +1,9 @@
-#!bin/sh
+#!bin/bash
 cargo b --release
 sudo setcap cap_net_admin=eip target/release/tcp-rust
 target/release/tcp-rust &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun_0
 sudo ip link set up dev tun_0
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// extern crate tun_tap;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -6,10 +5,26 @@ fn main() -> io::Result<()> {
      * Create Virtual Interface.
      */
     let nic = tun_tap::Iface::new("tun_0", tun_tap::Mode::Tun)?;
+    // 1500 = MTU Usually Packet Size, 4 = Header Size
+
+    // Flags [2 bytes]
+    // Proto [2 bytes]
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if proto != 0x0800 {
+            // ignore ipv6
+            continue
+        }
+        eprintln!(
+            "read {} bytes (flags : {:x}, proto: {:x}): {:x?}",
+            nbytes - 4,
+            flags,
+            proto,
+            &buf[4..nbytes]
+        );
     }
     Ok(())
 }
