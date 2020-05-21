4e9a83adff013f7a780ce81d658144f2680f454b
flekystyley <flekystyley@gmail.com>
2020-05-21 22:14:22 +0900

    update main.rs;

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::io;
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
+mod tcp;
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 
 fn main() -> io::Result<()> {
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
     /*
      * Create Virtual Interface.
      */
@@ -12,19 +23,42 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x0800 {
+        let _eth_flag = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        // 0x800 is ipv6
+        if eth_proto != 0x0800 {
             // ignore ipv6
             continue
         }
-        eprintln!(
-            "read {} bytes (flags : {:x}, proto: {:x}): {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+        
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(ip_header) => {
+                let src = ip_header.source_addr();
+                let dst = ip_header.destination_addr();
+                let proto = ip_header.protocol();
+                if proto != 0x06 {
+                    // not tcp
+                    continue
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + ip_header.slice().len()..nbytes]) {
+                    Ok(tcp_header) => {
+                        let datai = 4 + ip_header.slice().len() + tcp_header.slice().len();
+                        connections.entry(Quad {
+                            src: (src, tcp_header.source_port()),
+                            dst: (dst, tcp_header.destination_port()),
+                        }).or_default().on_packet(ip_header, tcp_header, &buf[datai..nbytes]);
+                        // (srcip, srcport, dstip, dstport)
+                    }
+                    Err(e) => {
+                        eprintln!("ignoring weird tcp packet {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ignoring weired packet {:?}", e);
+            }
+        }
     }
     Ok(())
 }
