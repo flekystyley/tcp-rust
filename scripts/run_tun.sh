f656ac04a34e5eda0671b38b1b12ea482fd3eb4f
flekystyley <flekystyley@gmail.com>
2020-05-21 22:14:06 +0900

    fix run_tun.sh;

--- scripts/run_tun.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-cargo b --release
-sudo setcap cap_net_admin=eip target/release/tcp-rust
-target/release/tcp-rust &
-pid=$!
-sudo ip addr add 192.168.0.1/24 dev tun_0
-sudo ip link set up dev tun_0
-trap "kill $pid" INT TERM
-wait $pid
